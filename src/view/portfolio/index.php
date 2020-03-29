00d9acb9b3284bb75a5fd126ab5cc17027e6f075
Bavo Vanderghote <bavovanderghote@Bavos-MacBook-Pro.local>
2020-03-29 17:20:55 +0200

    made and styled home menu for mobile

--- src/view/portfolio/index.php
+++ src/view/portfolio/index.php
@@ -1,1 +1,18 @@
-<H2>home</H2>
+<H2 class="hide">home</H2>
+
+<nav class="menu">
+  <ul class="menu__elements">
+    <li class="menu__element menu__element--about"><a href="?bavo-says=hello-i-am-bavo">
+      <p>About me</p>
+      <img class="menu__image menu__image--about" src="./assets/me2.png" alt="bavo hanging">
+    </a></li>
+    <li class="menu__element menu__element--work"><a href="?bavo-says=i-made-this">
+      <p>Work</p>
+      <img class="menu__image menu__image--work" src="./assets/me1.png" alt="bavo in half">
+    </a></li>
+    <li class="menu__element menu__element--contact"><a href="?bavo-says=hi">Say hi</a></li>
+  </ul>
+</nav>
+
+<!-- <img class="menu__image menu__image--half" src="./assets/me1.png" alt="bavo in half">
+<img class="menu__image menu__image--hanging" src="./assets/me2.png" alt="bavo hanging"> -->
