1b9ecbcb3d7f9c8498a9e6ee316d0963b9cd36d3
Bavo Vanderghote <bavovanderghote@Bavos-MacBook-Pro.local>
2020-04-12 22:41:41 +0200

    finished base project

--- src/controller/PortfolioController.php
+++ src/controller/PortfolioController.php
@@ -13,12 +13,14 @@ class PortfolioController extends Controller {
 
   public function index() {
     $this->set('title', "Welcome!");
+    $this->set('description', "Bavo Vanderghote is a graphic designer, front end developer and the occasional motionographer minding his own business in his little internet corner.");
   }
 
   public function about() {
     $images = $this->photographyDAO->selectImages();
 
     $this->set('title', "Hi, I'm Bavo");
+    $this->set('description', "Have a gander at some of my favourite photographs and take a peek into my personal life.");
     $this->set('images', $images);
 
     if (strtolower($_SERVER['HTTP_ACCEPT']) == 'application/json') {
@@ -31,12 +33,14 @@ class PortfolioController extends Controller {
 
   public function contact() {
     $this->set('title', "Hello, how are you?");
+    $this->set('description', "I don't know you yet, or maybe I do, come say hi!");
   }
 
   public function work() {
     $projects = $this->portfolioDAO->selectProjects();
 
     $this->set('title', "I made this");
+    $this->set('description', "Sometimes I make stuff. Mainly digital products and motion videos, but there's more to come!");
     $this->set('projects', $projects);
 
     if (strtolower($_SERVER['HTTP_ACCEPT']) == 'application/json') {
--- src/view/portfolio/index.php
+++ src/view/portfolio/index.php
@@ -2,15 +2,15 @@
 
 <nav class="menu">
   <ul class="menu__elements">
-    <li class="menu__element menu__element--about"><a class="link" href="?bavo-says=hello-i-am-bavo">
+    <li class="menu__element menu__element--about svg-filter hover--data" data-alt="I don't bite 👹"><a class="link" href="?bavo-says=hello-i-am-bavo">
       <p>About me</p>
       <img class="image menu__image--about" src="./assets/me2.png" alt="bavo hanging">
     </a></li>
-    <li class="menu__element menu__element--work"><a class="link" href="?bavo-says=i-made-this">
+    <li class="menu__element menu__element--work svg-filter hover--data" data-alt="👷‍♂️🔨"><a class="link" href="?bavo-says=i-made-this">
       <p>Work</p>
       <img class="image menu__image--work" src="./assets/me1.png" alt="bavo in half">
     </a></li>
-    <li class="menu__element menu__element--contact"><a class="link" href="?bavo-says=hi">Say hi</a></li>
+    <li class="menu__element menu__element--contact hover--data" data-alt="👋"><a class="link svg-filter" href="?bavo-says=hi">Say hi</a></li>
   </ul>
 </nav>
 
